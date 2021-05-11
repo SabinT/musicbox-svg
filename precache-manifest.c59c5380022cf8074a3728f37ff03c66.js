f224a12a31cae5e8c33e083294ad7f85fa9bebfd
Sabin <sabintimalsena@gmail.com>
2021-05-10 20:07:34 -0700

    Updates

---
+++ precache-manifest.c59c5380022cf8074a3728f37ff03c66.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbb7de40c5c407cff88f222b72a674d",
+    "url": "/musicbox-svg/index.html"
+  },
+  {
+    "revision": "283283bdbd0190f97d52",
+    "url": "/musicbox-svg/static/css/2.9df4b80b.chunk.css"
+  },
+  {
+    "revision": "af1b860773353179237c",
+    "url": "/musicbox-svg/static/css/main.bb6957a3.chunk.css"
+  },
+  {
+    "revision": "283283bdbd0190f97d52",
+    "url": "/musicbox-svg/static/js/2.1b4cc2ee.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/musicbox-svg/static/js/2.1b4cc2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1b860773353179237c",
+    "url": "/musicbox-svg/static/js/main.dcd86a1e.chunk.js"
+  },
+  {
+    "revision": "f79e16c08e8b4d5bbb39",
+    "url": "/musicbox-svg/static/js/runtime-main.86ec12bb.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/musicbox-svg/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/musicbox-svg/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/musicbox-svg/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/musicbox-svg/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/musicbox-svg/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/musicbox-svg/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
